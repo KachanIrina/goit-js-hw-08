8b75b2aa41f6b09ef92dc783f0e790083e7dbc21
KachanIrina <106675851+KachanIrina@users.noreply.github.com>
2022-11-15 12:26:32 +0200

    Завершене дз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,36 @@
 console.log(`hello world`)
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+player.on('timeupdate', throttle ((function (data) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }), 1000));
+
+player.setCurrentTime(Number(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
+     seconds = evt.currentTarget.value;
+     
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+localStorage.removeItem("videoplayer-current-time")
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector(".feedback-form input");
+const messege = document.querySelector(".feedback-form textarea");
+const STORAGE_KEY = "feedback-form-state";
+
+const userData = {
+    "email": "",
+    "messege": ""
+}
+
+form.addEventListener("submit", onFormSubmit);
+email.addEventListener("input", throttle(onTextareaInput, 500));
+messege.addEventListener("input", throttle(onTextareaInput, 500));
+ 
+populateTextarea()
+
+function onFormSubmit(evt) {
+evt.preventDefault();
+
+evt.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(evt) {
+userData.email = email.value;
+userData.messege = messege.value;
+localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
+
+
+
+function populateTextarea() {
+    const saveMessenge = localStorage.getItem(STORAGE_KEY);
+    const parseData = JSON.parse(saveMessenge);
+
+    if(parseData) {
+        console.log(parseData)
+        email.value = parseData.email || ""
+        messege.value = parseData.messege || ""
+        }
+}
 
